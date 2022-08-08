c9b243a0c974a5e53cc713799bcc91a09f79a1f6
netanelsu <58628741+netanelsu@users.noreply.github.com>
2022-08-08 17:17:12 +0300

    Add files via upload

---
+++ shop-app/App.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import * as Font from 'expo-font';
+import { enableScreens } from 'react-native-screens';
+import AppLoading from 'expo-app-loading';
+import ReduxThunk from 'redux-thunk';
+
+import productReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
+import authReducer from './store/reducers/auth';
+
+import NavigationContainer from './navigation/NavigationContainer';
+
+// https://shop-app-firebase-1606-default-rtdb.firebaseio.com/
+
+enableScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });      
+};
+
+const rootReducer = combineReducers({
+  products: productReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  
+  if(!fontLoaded){
+    return(
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer />
+    </Provider>
+  );
+}
